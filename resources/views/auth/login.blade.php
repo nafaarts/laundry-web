6472f03ff163c3bb9ad6babde809a1430f8ac5bb
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2022-10-31 12:23:09 +0700

    fix login ui

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.guest')
 
 @section('content')
-    <form class="card card-md" action="{{ route('login') }}" method="post" autocomplete="off">
+    <form class="card p-2" action="{{ route('login') }}" method="post" autocomplete="off">
         @csrf
 
         <div class="card-body">
-            <h2 class="card-title text-center mb-4">{{ __('Login to your account') }}</h2>
-
+            <h5 class="card-title text-center mb-4">{{ __('Login to your account') }}</h5>
             <div class="mb-3">
-                <label class="form-label">{{ __('Email address') }}</label>
-                <input type="email" name="email" value="{{ old('email') }}"
-                    class="form-control @error('email') is-invalid @enderror" placeholder="{{ __('Enter email') }}" required
-                    autofocus tabindex="1">
+                <label for="email" class="form-label">{{ __('Email address') }}</label>
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror""
+                    id="email" placeholder="name@example.com" autofocus>
                 @error('email')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -27,13 +25,13 @@
                     @endif
                 </label>
                 <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                    placeholder="{{ __('Password') }}" required tabindex="2">
+                    placeholder="{{ __('Password') }}" tabindex="2">
                 @error('password')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
-            <div>
+            <div class="mb-3">
                 <label class="form-check">
                     <input type="checkbox" class="form-check-input" tabindex="3" name="remember" />
                     <span class="form-check-label">{{ __('Remember me on this device') }}</span>
